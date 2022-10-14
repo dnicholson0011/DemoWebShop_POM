e11147b9d6372f18830366e4859f21e8e6e34437
dnicholson0011 <81931232+dnicholson0011@users.noreply.github.com>
2022-10-14 09:49:00 -0500

    Add methods to Computing and Books Pages

--- src/main/java/pages/BooksPage.java
+++ src/main/java/pages/BooksPage.java
@@ -1,12 +1,29 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import pages.books.ComputingAndInternetPage;
 
 public class BooksPage {
 
     private WebDriver driver;
 
+    private By addToCartButton = By.className("button-2 product-box-add-to-cart-button");
+
     public BooksPage(WebDriver driver) {
         this.driver = driver;
+    }
+
+    public void clickAddToCartButton() {
+        driver.findElement(addToCartButton).click();
+    }
+
+    public ComputingAndInternetPage clickComputingAndInternet() {
+        clickXpath("//a[normalize-space()='Computing and Internet']");
+        return new ComputingAndInternetPage(driver);
+    }
+
+    public void clickXpath(String xpath) {
+        driver.findElement(By.xpath(xpath)).click();
     }
 }
--- src/main/java/pages/books/ComputingAndInternetPage.java
+++ src/main/java/pages/books/ComputingAndInternetPage.java
@@ -4,14 +4,23 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import pages.BooksPage;
 
-public class ComputingAndInternetPage extends BooksPage {
+public class ComputingAndInternetPage {
 
     private WebDriver driver;
 
     private By quanityField = By.id("addtocart_13_EnteredQuantity");
 
-    public ComputingAndInternetPage(WebDriver driver, WebDriver driver1) {
-        super(driver);
-        this.driver = driver1;
+    private By addToCartButton = By.id("add-to-cart-button-13");
+
+    public ComputingAndInternetPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void setQuanity(String quanity)  {
+        driver.findElement(quanityField).sendKeys(quanity);
+    }
+
+    public void clickAddToCartButton() {
+        driver.findElement(addToCartButton).click();
     }
 }
