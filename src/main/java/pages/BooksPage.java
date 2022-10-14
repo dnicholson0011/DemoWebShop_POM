e31269ff92154121c5a88a91a7f6d208915ac493
dnicholson0011 <81931232+dnicholson0011@users.noreply.github.com>
2022-10-13 20:39:20 -0500

    Add BooksPage

---
+++ src/main/java/pages/BooksPage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class BooksPage {
+
+    private WebDriver driver;
+
+    public BooksPage(WebDriver driver) {
+        this.driver = driver;
+    }
+}
--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -12,16 +12,25 @@ public class HomePage {
     }
 
     public LoginPage clickLogin() {
-        clickLink("ico-login");
+        clickClassName("ico-login");
         return new LoginPage(driver);
     }
 
     public RegistrationPage clickRegistration() {
-        clickLink("ico-register");
+        clickClassName("ico-register");
         return new RegistrationPage(driver);
     }
 
-    public void clickLink(String className) {
+    public BooksPage clickBooks() {
+        clickCssSelector("");
+        return new BooksPage(driver);
+    }
+
+    public void clickClassName(String className) {
         driver.findElement(By.className(className)).click();
+    }
+
+    public void clickCssSelector(String cssSelector) {
+        driver.findElement(By.cssSelector(cssSelector)).click();
     }
 }
